61c783ce4c6bf5e7fa3f6aef5ee66716474c2081
MrGhareeb <alighareeb99@gmail.com>
2021-12-07 17:30:08 +0300

    setup admin panel

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class UserController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'User';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User());
+
+        $grid->column('id', __('Id'));
+        $grid->column('name', __('Name'));
+        $grid->column('email', __('Email'));
+        $grid->column('email_verified_at', __('Email verified at'));
+        $grid->column('password', __('Password'));
+        $grid->column('private', __('Private'));
+        $grid->column('user_image', __('User image'));
+        $grid->column('mobile_number', __('Mobile number'));
+        $grid->column('remember_token', __('Remember token'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+        $grid->column('user_type_id', __('User type id'));
+        $grid->column('user_status_id', __('User status id'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('name', __('Name'));
+        $show->field('email', __('Email'));
+        $show->field('email_verified_at', __('Email verified at'));
+        $show->field('password', __('Password'));
+        $show->field('private', __('Private'));
+        $show->field('user_image', __('User image'));
+        $show->field('mobile_number', __('Mobile number'));
+        $show->field('remember_token', __('Remember token'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+        $show->field('user_type_id', __('User type id'));
+        $show->field('user_status_id', __('User status id'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new User());
+
+        $form->text('name', __('Name'));
+        $form->email('email', __('Email'));
+        $form->datetime('email_verified_at', __('Email verified at'))->default(date('Y-m-d H:i:s'));
+        $form->password('password', __('Password'));
+        $form->switch('private', __('Private'));
+        $form->text('user_image', __('User image'));
+        $form->text('mobile_number', __('Mobile number'));
+        $form->text('remember_token', __('Remember token'));
+        $form->number('user_type_id', __('User type id'));
+        $form->number('user_status_id', __('User status id'));
+
+        return $form;
+    }
+}
