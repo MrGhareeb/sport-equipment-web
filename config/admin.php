096500309e094b5f5e5cbfe3dd4f39492a8e43d5
MrGhareeb <alighareeb99@gmail.com>
2021-12-08 14:57:48 +0300

    adjust the default image

--- config/admin.php
+++ config/admin.php
@@ -11,7 +11,7 @@ return [
     | login page.
     |
     */
-    'name' => 'Mafqoud',
+    'name' => 'Laravel-admin',
 
     /*
     |--------------------------------------------------------------------------
@@ -249,7 +249,7 @@ return [
     | Set a default avatar for newly created users.
     |
     */
-    'default_avatar' => '/vendor/laravel-admin/AdminLTE/dist/img/user2-160x160.jpg',
+    'default_avatar' => '/img/placeholder.png',
 
     /*
     |--------------------------------------------------------------------------
