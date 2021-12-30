a0971600d06835c38ecee64711850b4d18606041
MrGhareeb <alighareeb99@gmail.com>
2021-12-30 10:47:10 +0300

    fix the profile and edit project page

--- resources/views/editProfile.blade.php
+++ resources/views/editProfile.blade.php
@@ -5,7 +5,7 @@
     <div class="container mx-auto my-5 p-5">
         <script>
             function deleteUser() {
-               var form = document.getElementById("editProfileForm");
+                var form = document.getElementById("editProfileForm");
                 form.action = "{{ route('deleteUser') }}";
                 form.method = "POST";
                 form.submit();
@@ -89,7 +89,7 @@
                                     <div class="px-4 py-2 font-semibold">Contact No.</div>
                                     <div class="px-4 py-2">
                                         <div class=""">
-                                                            <input type=" text"
+                                                                <input type=" text"
                                             class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600 bg-white"
                                             value="{{ $user->mobile_number }}" style="margin-top:-1em"
                                             name="mobile_number">
@@ -173,53 +173,7 @@
 
                     <div class="my-4"></div>
 
-                    <!-- Experience and education -->
-                    <div class="bg-white p-3 shadow-sm rounded-sm">
 
-                        <div class="grid grid-cols-2">
-                            <div>
-                                <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-3">
-                                    <span clas="text-green-500">
-                                        {{-- <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z">
-                                        </path>
-                                    </svg> --}}
-                                    </span>
-                                    <span class="tracking-wide">Test</span>
-                                </div>
-                                <ul class="list-inside space-y-2">
-                                    <li>
-                                        <div class="text-teal-600">test</div>
-                                        <div class="text-gray-500 text-xs">test</div>
-                                    </li>
-
-                                </ul>
-                            </div>
-                            <div>
-                                <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 mb-3">
-                                    <span clas="text-green-500">
-                                        {{-- <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path fill="#fff" d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                                        <path fill="#fff"
-                                            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z">
-                                        </path>
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222">
-                                        </path>
-                                    </svg> --}}
-                                    </span>
-                                    <span class="tracking-wide">Test</span>
-                                </div>
-                                <ul class="list-inside space-y-2">
-
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- End of Experience and education grid -->
-                    </div>
                     <!-- End of Experience and education grid -->
                 </div>
                 <!-- End of profile tab -->
